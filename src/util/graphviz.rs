2747cf60b7e7c0b29a491aa85b460f6c897db49d
Sam Stites <stites@users.noreply.github.com>
2022-08-25 21:53:42 -0400

    isolate rust bug, pt I

--- src/util/graphviz.rs
+++ src/util/graphviz.rs
@@ -7,7 +7,7 @@ use std::collections::VecDeque;
 
 use crate::builder::bdd_builder::BddManager;
 use crate::builder::bdd_plan::BddPlan;
-use crate::builder::repr::builder_bdd::BddPtr;
+use crate::builder::repr::builder_bdd::{BddPtr, PointerType};
 use crate::builder::repr::builder_sdd::{SddPtr, VTree};
 use crate::builder::*;
 use crate::repr::var_label::VarLabel;
@@ -317,5 +317,250 @@ mod test_graphviz {
             T[shape=rectangle]
             F[shape=rectangle]
         }";
+    }
+
+    #[derive(Debug, Clone, PartialEq, Eq)]
+    pub struct BddNode {
+        pub low: BddPtr,
+        pub high: BddPtr,
+        pub var: VarLabel,
+    }
+
+    impl BddNode {
+        pub fn new(low: BddPtr, high: BddPtr, var: VarLabel) -> BddNode {
+            BddNode {
+                low: low,
+                high: high,
+                var: var,
+            }
+        }
+    }
+
+    #[derive(Debug, Clone, PartialEq, Eq)]
+    pub enum Bdd {
+        Node(BddNode),
+        BddTrue,
+        BddFalse,
+    }
+
+    impl Bdd {
+        pub fn new_node(low: BddPtr, high: BddPtr, var: VarLabel) -> Bdd {
+            let new_n = BddNode {
+                low: low,
+                high: high,
+                var: var,
+            };
+            Bdd::Node(new_n)
+        }
+
+        pub fn into_node(&self) -> BddNode {
+            match self {
+                &Bdd::Node(ref n) => n.clone(),
+                _ => panic!("called into-node on non-node BDD"),
+            }
+        }
+    }
+
+
+    /// The primary BDD storage object
+    /// Called 'Topless' because it does not keep keep track of the top variable (as
+    /// this is implicit in the storage table)
+    #[derive(Debug, Clone, Copy, Eq)]
+    pub struct ToplessBdd {
+        pub low: BddPtr,
+        pub high: BddPtr,
+    }
+
+    impl PartialEq for ToplessBdd {
+        fn eq(&self, other: &ToplessBdd) -> bool {
+            // Ignore the scratch space when checking equality
+            self.low == other.low && self.high == other.high
+        }
+    }
+
+    impl ToplessBdd {
+        pub fn new(low: BddPtr, high: BddPtr) -> ToplessBdd {
+            ToplessBdd {
+                low: low,
+                high: high,
+            }
+        }
+    }
+
+    use crate::builder::repr::builder_bdd::TableIndex;
+
+    #[derive(Clone)]
+    /// The primary storage unit for binary decision diagram nodes
+    /// Each variable is associated with an individual subtable
+    pub struct BddTable {
+        subtables: Vec<Vec<ToplessBdd>>,
+    }
+
+    impl BddTable {
+        pub fn new(num_vars: usize) -> BddTable {
+            let mut v = Vec::with_capacity(num_vars);
+            for _ in 0..num_vars {
+                v.push(vec![]);
+            }
+
+            BddTable {
+                subtables: v,
+            }
+        }
+        pub fn get_or_insert(&mut self, bdd: Bdd) -> BddPtr {
+            match bdd {
+                Bdd::BddFalse => BddPtr::false_node(),
+                Bdd::BddTrue => BddPtr::true_node(),
+                Bdd::Node(n) => {
+                    let var = n.var.value();
+                    let elem = ToplessBdd::new(n.low, n.high);
+                    let vartable = &self.subtables[var as usize];
+                    let tptr = match vartable.iter().filter(|e| **e == elem).next() {
+                        None => {
+                            self.subtables[var as usize].push(elem);
+                            elem
+                        }
+                        Some(ptr) => *ptr,
+                    };
+
+                    BddPtr::new(VarLabel::new(var), TableIndex::new(0 as u64))
+                }
+            }
+        }
+
+        pub fn deref(&self, ptr: BddPtr) -> Bdd {
+            match ptr.ptr_type() {
+                PointerType::PtrFalse => Bdd::BddFalse,
+                PointerType::PtrTrue => Bdd::BddTrue,
+                PointerType::PtrNode => {
+                    let topless =
+                        self.subtables[ptr.var() as usize][0];
+                    Bdd::new_node(topless.low, topless.high, VarLabel::new(ptr.var()))
+                }
+            }
+        }
+
+    }
+
+    pub struct MyBddManager {
+        compute_table: BddTable,
+        order: Vec<VarLabel>,
+    }
+    impl MyBddManager {
+        pub fn new(order: Vec<VarLabel>) -> MyBddManager {
+            MyBddManager {
+                compute_table: BddTable::new(order.len()),
+                order,
+            }
+        }
+        pub fn var(&mut self, lbl: VarLabel) -> BddPtr {
+            let bdd = BddNode::new(BddPtr::false_node(), BddPtr::true_node(), lbl);
+            let r = self.get_or_insert(bdd);
+            r
+
+        }
+        pub fn low(&self, ptr: BddPtr) -> BddPtr {
+            assert!(!ptr.is_const(), "Attempting to get low pointer of constant BDD");
+            let b = self.compute_table.deref(ptr).into_node();
+            b.low
+        }
+        pub fn high(&self, ptr: BddPtr) -> BddPtr {
+            assert!(!ptr.is_const(), "Attempting to get high pointer of constant BDD");
+            let b = self.compute_table.deref(ptr).into_node();
+            b.high
+        }
+        fn get_or_insert(&mut self, bdd: BddNode) -> BddPtr {
+           let bdd = Bdd::new_node(bdd.low, bdd.high, bdd.var);
+           self.compute_table.get_or_insert(bdd)
+        }
+
+        pub fn and(&mut self, f: BddPtr, g: BddPtr) -> BddPtr {
+            println!("and!");
+            let reg_f = f.regular();
+            let reg_g = g.regular();
+
+            // we only deal with true in this example.
+            if reg_f.is_true() {
+                if f.is_true() {
+                    return g;
+                } else {
+                    return f;
+                }
+            }
+
+            let index = f.label();
+            let fv = self.high(reg_f);
+            let fnv = self.low(reg_f);
+            let gv = g;
+            let gnv = g;
+
+            // now recurse
+            let new_h = self.and(fv, gv);
+            let new_l = self.and(fnv, gnv);
+            let n = BddNode {
+                low: new_l,
+                high: new_h,
+                var: index,
+            };
+            let r = self.get_or_insert(n);
+            return r;
+        }
+    }
+
+    fn my_get_label(mgr: &MyBddManager, ptr: BddPtr) -> (String, PointerType) {
+        let x = ptr.ptr_type().clone();
+        match x {
+           PtrTrue  => ("PtrTrue".to_string(), x),
+           PtrFalse => ("PtrFalse".to_string(), x),
+           PtrNode  => ("PtrNode".to_string(), x),
+       }
+    }
+    type MyReturn = Vec<((BddPtr, (String, PointerType)), (BddPtr, (String, PointerType)))>;
+    pub fn call_recursive_bfs(mgr: &MyBddManager, ptr: BddPtr) -> MyReturn {
+        use crate::builder::repr::builder_bdd::PointerType::*;
+        fn bfs_recursively(
+            mgr: &MyBddManager,
+            optr: Option<BddPtr>,
+            queue: &mut Vec<BddPtr>,
+            edges: &mut MyReturn,
+        ) -> MyReturn {
+            match optr {
+                None => edges.to_vec(),
+                Some(ptr) => {
+                    match ptr.ptr_type() {
+                        PtrTrue  => bfs_recursively(mgr, queue.pop(), queue, edges),
+                        PtrFalse => bfs_recursively(mgr, queue.pop(), queue, edges),
+                        PtrNode => {
+                            let lp = mgr.low(ptr);
+                            let hp = mgr.high(ptr);
+                            let parent = (ptr, my_get_label(mgr, ptr));
+                            let c1 = (lp, my_get_label(mgr, lp));
+                            let c2 = (hp, my_get_label(mgr, hp));
+                            edges.extend(vec![
+                                (parent.clone(), c1),
+                                (parent.clone(), c2),
+                            ]);
+                            queue.extend(vec![hp]);
+                            bfs_recursively(mgr, Some(lp), queue, edges)
+                        }
+                    }
+                }
+            }
+        }
+        bfs_recursively(mgr, Some(ptr), &mut vec![], &mut vec![],)
+    }
+
+
+    #[test]
+    fn test_dot_rust_bug() {
+        let va = VarLabel::new(0);
+        let vb = VarLabel::new(1);
+        let mut mgr = MyBddManager::new(vec![va, vb]);
+        let a = mgr.var(va);
+        let b = mgr.var(vb);
+        let ab = mgr.and(a, b);
+
+        println!("{:#?}", call_recursive_bfs(&mgr, a));
+        todo!();
     }
 }
