7927b5f1cbbecbf57ebcdb223b4341a0f993fbc5
Sam Stites <stites@users.noreply.github.com>
2023-10-21 15:06:14 -0400

    include ffi and cli

--- nix/default.nix
+++ nix/default.nix
@@ -9,9 +9,13 @@ rustPlatform.buildRustPackage rec {
 
   src = ../.;
 
-  cargoHash = "sha256-GYO554P3VHilJm2z/BM8noiPKG/HQM5jSELwuXAHJAA=";
+  #cargoHash = "sha256-GYO554P3VHilJm2z/BM8noiPKG/HQM5jSELwuXAHJAA=";
+  cargoHash = "sha256-dl1R1VHPPwa2RVZw3as7zbmKUSrWvjTrMYn/YEYw/38=";
 
   cargoPatches = [./0001-Cargo.lock.patch];
+
+
+  buildFeatures = [ "ffi" "cli" ];
 
   meta = with lib; {
     description = "Rust decision diagrams";
